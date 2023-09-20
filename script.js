4897e1d352f08875b17c1ae11e5b94c87cf36f58
Lokendra Bhat <lokendrabhat.2022@gmail.com>
2023-09-20 15:44:58 +0545

    new items are added

---
+++ script.js
@@ -0,0 +1,12 @@
+var sidebar = document.getElementById("sidemenu");
+
+
+// To open sidemenu
+function openMenu(){
+    sidebar.style.right = "0";
+}
+
+// To close sidemenu
+function closeMenu(){
+    sidebar.style.right = "-200px";
+}
